ca7bd7154a5d5b4fd05b95cdeafe671abf244cfd
lgerardlucas <lgerardlucas@gmail.com>
2020-02-06 23:22:55 -0300

    Melhoria no processo de inicio de cotação por parte do cliente, e ajustes e criação dos acessos as redes sociais

--- qnow_site/static/bootstrap/js/custom.js
+++ qnow_site/static/bootstrap/js/custom.js
@@ -6,6 +6,10 @@ window.onload = function() {
     const district = document.getElementById('id_district');
     const approved = document.getElementById('info_approved');
     const contractAcceptance = document.getElementById('id_contract_accepted');
+    const mobileType = document.getElementById('id_mobile_type');
+    const mobileDescription = document.getElementById('id_mobile_description');
+    const particulars = document.getElementById('id_particulars');
+    
 
     $('#id_cep').mask('00000-000');
     $('#id_phone').mask('(00) 00000-0000');
@@ -21,7 +25,7 @@ window.onload = function() {
     });
 
     
-    if (approved){
+    if (approved) {
         approved.addEventListener('click', function (event) {
             const target = event.currentTarget;
             const value = target.innerText;
@@ -29,7 +33,93 @@ window.onload = function() {
         });
     }
 
-
+    if (mobileType) {
+        mobileType.addEventListener('change', function (event) {
+            var x = mobileType.value;
+            /*ID = 1_Para o tipo de móvel Janela */
+            if (x == 1) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Tamanho da janela:\n'+
+                                    '2) Ambiente a ser instalada:\n'+
+                                    '3) Material desejado:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 2_Para o tipo de móvel Mesa */
+            else if (x == 2) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Nº de Lugares:\n'+
+                                    '2) Tampo de MDF ou Granito:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 3_Para o tipo de móvel Painel */
+            else if (x == 3) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Largura e altura:\n'+
+                                    '2) Ambiente a ser instalado:\n'+
+                                    '3) Nichos e prateleiras:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 4_Para o tipo de móvel Banheiro */
+            else if (x == 4) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Largura e altura:\n'+
+                                    '2) Com ou sem espelho:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 5_Para o tipo de móvel Porta */
+            else if (x == 5) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Ambiente a ser instalada:\n'+
+                                    '2) Opção de abertura(Bater ou Correr):\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 6_Para o tipo de móvel Roupeiro */
+            else if (x == 6) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Tamanho do ambiente:\n'+
+                                    '2) Portas de bater ou Correr:\n'+
+                                    '3) Reto ou canto:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 7_Para o tipo de móvel Cozinha */
+            else if (x == 7) {
+                particulars.value = 'Informações:\n'+
+                                    '1) Tamanho do ambiente:\n'+
+                                    '2) Torre ou nichos:\n'+
+                                    '3) Fogão cooktop ou comum:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 8_Para o tipo de móvel não indentificado */
+            else if (x == 8) {
+                mobileDescription.value = '*** Digite o nome do móvel aqui ***';
+                particulars.value = 'Informações:\n'+
+                                    '1) Ambiente a ser instalado:\n'+
+                                    '2) Tamanho do ambiente:\n'+
+                                    '\n'+
+                                    'Observações:';
+            }
+            /*ID = 9_Para o tipo de móvel Churrasqueira */
+            else if (x == 9) {
+                particulars.value = 'Observações:';
+            }
+            /*ID = 10_Envio do projeto por e-mail */
+            else if (x == 10) {
+                mobileDescription.value = 'Enviarei um e-mail após salvar esta cotação!';
+                particulars.value = 'Para o tipo de opção "_Projeto por e-mail" logo depois de salvar esta cotação, é necessário enviar um email para mgacotações@gmail.com para que possa ser enviado a todos os marceneiros seu projeto e você receber orçamentos.';
+            }    
+            else { 
+                mobileDescription.value = '';
+            }
+        });
+    }   
+     
     if (cep) {
         cep.onkeyup = function(event) {
             const value = event.target.value.replace('-', '');
@@ -71,4 +161,5 @@ window.onload = function() {
             }
         });
     }
+
 }
