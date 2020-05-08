3d364cfbb24b89a6e7a5a5c47cba8f2223915f62
lgerardlucas <lgerardlucas@gmail.com>
2020-05-07 22:07:41 -0300

    Inclusão de slider

--- qnow_site/static/bootstrap/js/custom.js
+++ qnow_site/static/bootstrap/js/custom.js
@@ -11,22 +11,22 @@ window.onload = function() {
     const particulars = document.getElementById('id_particulars');
     const cityList = document.getElementById('id_city_list');
     const stateList = document.getElementById('id_state_list');
-    
+
 
     $('#id_cep').mask('00000-000');
     $('#id_phone').mask('(00) 00000-0000');
     $('#id_cpf').mask('000.000.000-00', { reverse: true });
     $('#id_cnpj').mask('00.000.000/0000-00', { reverse: true });
-    $('.dinheiro').mask('#####0.00', { reverse: true } ); 
+    $('.dinheiro').mask('#####0.00', { reverse: true });
     $('.date_format').mask('00/00/0000');
     $('form').on('submit', (event) => {
         const submit = $(event.target).find('input[type="submit"]');
         submit.attr('disabled', 'disabled').val('Processando e enviando um email, aguarde...');
     });
 
-    
+
     if (approved) {
-        approved.addEventListener('click', function (event) {
+        approved.addEventListener('click', function(event) {
             const target = event.currentTarget;
             const value = target.innerText;
             target.innerText = value.includes('Fechar') ? value.replace('Fechar ', '') : `Fechar ${value}`;
@@ -36,76 +36,76 @@ window.onload = function() {
 
     /*Comando para quando trocar o valor do campo mobileType preencher outros campos com caracteristicas proprias */
     if (mobileType) {
-        mobileType.addEventListener('change', function () {
+        mobileType.addEventListener('change', function() {
             var x = mobileType.value;
             /*ID = 1_Para o tipo de móvel Janela */
             if (x == 1) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Tamanho da janela:\n'+
-                                    '2) Ambiente a ser instalada:\n'+
-                                    '3) Material desejado:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Tamanho da janela:\n' +
+                    '2) Ambiente a ser instalada:\n' +
+                    '3) Material desejado:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 2_Para o tipo de móvel Mesa */
             else if (x == 2) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Nº de Lugares:\n'+
-                                    '2) Tampo de MDF ou Granito:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Nº de Lugares:\n' +
+                    '2) Tampo de MDF ou Granito:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 3_Para o tipo de móvel Painel */
             else if (x == 3) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Largura e altura:\n'+
-                                    '2) Ambiente a ser instalado:\n'+
-                                    '3) Nichos e prateleiras:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Largura e altura:\n' +
+                    '2) Ambiente a ser instalado:\n' +
+                    '3) Nichos e prateleiras:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 4_Para o tipo de móvel Banheiro */
             else if (x == 4) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Largura e altura:\n'+
-                                    '2) Com ou sem espelho:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Largura e altura:\n' +
+                    '2) Com ou sem espelho:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 5_Para o tipo de móvel Porta */
             else if (x == 5) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Ambiente a ser instalada:\n'+
-                                    '2) Opção de abertura(Bater ou Correr):\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Ambiente a ser instalada:\n' +
+                    '2) Opção de abertura(Bater ou Correr):\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 6_Para o tipo de móvel Roupeiro */
             else if (x == 6) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Tamanho do ambiente:\n'+
-                                    '2) Portas de bater ou Correr:\n'+
-                                    '3) Reto ou canto:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Tamanho do ambiente:\n' +
+                    '2) Portas de bater ou Correr:\n' +
+                    '3) Reto ou canto:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 7_Para o tipo de móvel Cozinha */
             else if (x == 7) {
-                particulars.value = 'Informações:\n'+
-                                    '1) Tamanho do ambiente:\n'+
-                                    '2) Torre ou nichos:\n'+
-                                    '3) Fogão cooktop ou comum:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Tamanho do ambiente:\n' +
+                    '2) Torre ou nichos:\n' +
+                    '3) Fogão cooktop ou comum:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 8_Para o tipo de móvel não indentificado */
             else if (x == 8) {
                 mobileDescription.value = '*** Digite o nome do móvel aqui ***';
-                particulars.value = 'Informações:\n'+
-                                    '1) Ambiente a ser instalado:\n'+
-                                    '2) Tamanho do ambiente:\n'+
-                                    '\n'+
-                                    'Observações:';
+                particulars.value = 'Informações:\n' +
+                    '1) Ambiente a ser instalado:\n' +
+                    '2) Tamanho do ambiente:\n' +
+                    '\n' +
+                    'Observações:';
             }
             /*ID = 9_Para o tipo de móvel Churrasqueira */
             else if (x == 9) {
@@ -115,31 +115,30 @@ window.onload = function() {
             else if (x == 10) {
                 mobileDescription.value = 'Envie um e-mail com seu projeto';
                 particulars.value = 'Para o tipo de opção "_Projeto por e-mail" salve esta cotação e envie um email para mgacotações@gmail.com com seu projeto.';
-            }    
+            }
             /*ID = 11_Reforma de um móvel */
             else if (x == 11) {
                 mobileDescription.value = 'Indique aqui o móvel a ser reformado';
-                particulars.value = 'Informações:\n'+
-                                    '1) Tipo de produção, marcenaria ou Loja?\n'+
-                                    '2) Qual a urgência?\n'+
-                                    '3) Importante: Inclua fotos do móvel!\n'+
-                                    '\n'+
-                                    'Observações sobre o tipo de reforma:';
-            }    
-            else { 
+                particulars.value = 'Informações:\n' +
+                    '1) Tipo de produção, marcenaria ou Loja?\n' +
+                    '2) Qual a urgência?\n' +
+                    '3) Importante: Inclua fotos do móvel!\n' +
+                    '\n' +
+                    'Observações sobre o tipo de reforma:';
+            } else {
                 mobileDescription.value = '';
             }
         });
-    }   
-     
+    }
+
     if (cep) {
         cep.onkeyup = function(event) {
             const value = event.target.value.replace('-', '');
-    
+
             if (value.length < 8) return false;
-    
+
             const path = `https://viacep.com.br/ws/${value}/json/`;
-    
+
             fetch(path)
                 .then(response => response.json())
                 .then(data => {
@@ -163,7 +162,7 @@ window.onload = function() {
     if (contractAcceptance) {
         const submitButton = document.querySelector('input[type=submit]');
 
-        contractAcceptance.addEventListener('change', function (event) {
+        contractAcceptance.addEventListener('change', function(event) {
             const { checked } = event.target;
 
             if (checked) {
@@ -183,21 +182,36 @@ window.onload = function() {
     A mesma ideia eu fiz para o Estado, mudando o valor da cidade quando o seletor estiver diferente do default.
     */
     if (cityList) {
-        cityList.addEventListener('change', function () {
+        cityList.addEventListener('change', function() {
             if (cityList.value != 'Cidade') {
-               stateList.getElementsByTagName('option')[stateList.length-1].selected = 'selected';
+                stateList.getElementsByTagName('option')[stateList.length - 1].selected = 'selected';
             }
         });
-    }    
+    }
 
     if (stateList) {
-        stateList.addEventListener('change', function () {
+        stateList.addEventListener('change', function() {
             if (stateList.value != 'Estado') {
-                cityList.getElementsByTagName('option')[cityList.length-1].selected = 'selected';
+                cityList.getElementsByTagName('option')[cityList.length - 1].selected = 'selected';
             }
         });
-    }    
+    }
 
+
+    const mySwiper = new Swiper('.swiper-projects', {
+        slidesPerView: 'auto',
+        /*         centeredSlides: true,
+         */
+        spaceBetween: 5,
+        navigation: {
+            nextEl: '.swiper-projects .swiper-button-next',
+            prevEl: '.swiper-projects .swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-projects .swiper-pagination',
+            clickable: true,
+        },
+    });
 
 }
 
