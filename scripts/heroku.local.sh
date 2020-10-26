759ca3ad83d1cf2cfab489ee83a3373bdfb01bec
Marcos André Gerard Alves <lgerardlucas@gmail.com>
2020-10-26 18:36:53 -0300

    Delete heroku.local.sh

--- scripts/heroku.local.sh
+++
@@ -1,6 +0,0 @@
-echo
-echo "=========== Executando com heroku CLI =========="
-echo "Executando o site usando o heroku CLI localmente - Comando: heroku local -e .env -p 8000"
-heroku local -e .env -p 8000
-echo
-echo "==================== Fim ======================="
